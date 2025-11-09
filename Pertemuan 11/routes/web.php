94536e9815099f59f01e606b75a0298a9d6e5743
EsKopikkk <riyadhyhaadiy@gmail.com>
2025-11-09 10:44:50 +0800

    Push Folder UTS yang sudah di laravel-kan

---
+++ Pertemuan 11/routes/web.php
@@ -0,0 +1,110 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('index');
+});
+
+Route::get('/about', function () {
+    return view('aboutme');
+});
+
+
+// ===== AUTO-GENERATED ROUTES =====
+Route::get('/Pudding1', function () {
+    return view('Pudding1');
+});
+
+Route::get('/Pudding2', function () {
+    return view('Pudding2');
+});
+
+Route::get('/aboutme', function () {
+    return view('aboutme');
+});
+
+Route::get('/ayamBakar', function () {
+    return view('ayamBakar');
+});
+
+Route::get('/ayamChick', function () {
+    return view('ayamChick');
+});
+
+Route::get('/ayamKari', function () {
+    return view('ayamKari');
+});
+
+Route::get('/ayamKuning', function () {
+    return view('ayamKuning');
+});
+
+Route::get('/bolu1', function () {
+    return view('bolu1');
+});
+
+Route::get('/bolu2', function () {
+    return view('bolu2');
+});
+
+Route::get('/bolu3', function () {
+    return view('bolu3');
+});
+
+Route::get('/halo', function () {
+    return view('halo');
+});
+
+Route::get('/index', function () {
+    return view('index');
+});
+
+Route::get('/kering1', function () {
+    return view('kering1');
+});
+
+Route::get('/kering2', function () {
+    return view('kering2');
+});
+
+Route::get('/kering3', function () {
+    return view('kering3');
+});
+
+Route::get('/kering4', function () {
+    return view('kering4');
+});
+
+Route::get('/kueBolu', function () {
+    return view('kueBolu');
+});
+
+Route::get('/kueKering', function () {
+    return view('kueKering');
+});
+
+Route::get('/lauk', function () {
+    return view('lauk');
+});
+
+Route::get('/r1', function () {
+    return view('r1');
+});
+
+Route::get('/r2', function () {
+    return view('r2');
+});
+
+Route::get('/r3', function () {
+    return view('r3');
+});
+
+Route::get('/roti', function () {
+    return view('roti');
+});
+
+Route::get('/roti1', function () {
+    return view('roti1');
+});
+
